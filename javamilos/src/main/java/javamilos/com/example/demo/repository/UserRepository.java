ee70a562f6a911669ff62d0238481519028494a4
milos <milos.merdovic1@gmail.com>
2020-10-12 16:59:22 +0200

    first changes

---
+++ javamilos/src/main/java/javamilos/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package javamilos.com.example.demo.repository;
+
+import javamilos.com.example.demo.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface UserRepository extends CrudRepository<User,Long> {
+
+    List<User> findByName(String name);
+}

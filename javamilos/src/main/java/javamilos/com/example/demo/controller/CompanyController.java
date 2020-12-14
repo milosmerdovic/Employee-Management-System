fc780d58a41277c5f92947ecaa69c851b1d89973
milos <milos.merdovic1@gmail.com>
2020-12-14 10:38:02 +0100

    Putanje izmenjene

--- javamilos/src/main/java/javamilos/com/example/demo/controller/CompanyController.java
+++ javamilos/src/main/java/javamilos/com/example/demo/controller/CompanyController.java
@@ -22,53 +22,53 @@ public class CompanyController {
     }
     @GetMapping("/company")
     public String showCompanyForm(Company company) {
-        return "company";
+        return "company/company";
     }
 
     @GetMapping("/create")
     public String showSignUpForm(Company company) {
-        return "add-company";
+        return "company/add-company";
     }
 
     @PostMapping("/add-company")
     public String addCompany(@Valid Company company, BindingResult result, Model model) {
         if (result.hasErrors()) {
-            return "/company/index";
+            return "add-company";
         }
         companyRepository.save(company);
         model.addAttribute("companies", companyRepository.findAll());
         return "redirect:/company/index";
     }
 
-    @GetMapping(value = {"/company","/edit-company/{id}"})
+    @GetMapping("/company/edit-company/{id}")
     public String showEditForm(@PathVariable("id") long id, Model model) {
         Company company = companyRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid company Id:" + id));
         model.addAttribute("company", company);
-        return "update-company";
+        return "company/update-company";
     }
 
-    @PostMapping("/update-company/{id}")
+    @PostMapping("/company/update-company/{id}")
     public String updateCompany(@PathVariable("id") long id, @Valid Company company, BindingResult result, Model model) {
         if (result.hasErrors()) {
             company.setId(id);
-            return "update-company";
+            return "/main/resources/templates/companies/update-company.html";
         }
         companyRepository.save(company);
         model.addAttribute("companies", companyRepository.findAll());
-        return "redirect:/company/index";
+        return "company/index";
     }
 
-    @GetMapping(value = {"/company","/delete-company/{id}"})
+    @GetMapping("/company/delete-company/{id}")
     public String deleteCompany(@PathVariable("id") long id, Model model) {
         Company company = companyRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid company Id:" + id));
         companyRepository.delete(company);
         model.addAttribute("companies", companyRepository.findAll());
-        return "/company/index";
+        return "company/index";
     }
 
-    @GetMapping(value = {"/company","/index"})
+    @GetMapping("/company/index")
     public String allCompanies(Model model){
         model.addAttribute("companies", companyRepository.findAll());
-        return "/company/index";
+        return "company/index";
     }
 }
--- javamilos/src/main/java/javamilos/com/example/demo/controller/UserController.java
+++ javamilos/src/main/java/javamilos/com/example/demo/controller/UserController.java
@@ -22,55 +22,56 @@ public class UserController {
         this.userRepository = userRepository;
     }
     @GetMapping("/user")
-    public String showUserForm(User user) {
-        return "user";
+    public String showUserForm(Model model){
+        model.addAttribute("users", userRepository.findAll());
+        return "user/user";
     }
 
     @GetMapping("/signup")
     public String showSignUpForm(User user) {
-        return "add-user";
+        return "user/add-user";
     }
 
     @PostMapping("/add-user")
     public String addUser(@Valid User user, BindingResult result, Model model) {
         if (result.hasErrors()) {
-            return "add-user";
+            return "user/add-user";
         }
         userRepository.save(user);
         model.addAttribute("users", userRepository.findAll());
-        return "redirect:/user/index";
+        return "user/index";
     }
 
-    @GetMapping(value = {"/user","/edit/{id}"})
+    @GetMapping("/user/edit/{id}")
     public String showUpdateForm(@PathVariable("id") long id, Model model) {
         User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
         model.addAttribute("user", user);
-        return "update-user";
+        return "user/update-user";
     }
 
-    @PostMapping(value = {"/user","/update/{id}"})
+    @PostMapping("/user/update/{id}")
     public String updateUser(@PathVariable("id") long id, @Valid User user, BindingResult result, Model model) {
         if (result.hasErrors()) {
             user.setId(id);
-            return "update-user";
+            return "user/update-user";
         }
         userRepository.save(user);
         model.addAttribute("users", userRepository.findAll());
-        return "redirect:/user/index";
+        return "user/index";
     }
 
-    @GetMapping(value = {"/user","/delete/{id}"})
+    @GetMapping("/user/delete/{id}")
     public String deleteUser(@PathVariable("id") long id, Model model) {
         User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
         userRepository.delete(user);
         model.addAttribute("users", userRepository.findAll());
-        return "/user/index";
+        return "user/index";
     }
 
-    @GetMapping(value = {"/user","/index"})
+    @GetMapping("/user/index")
     public String allUsers(Model model){
         model.addAttribute("users", userRepository.findAll());
-                return "/user/index";
+                return "user/index";
     }
 
 }

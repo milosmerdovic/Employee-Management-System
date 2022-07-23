d9259adaca1cfaf1e0e6d6d114c9b0485805c2b5
milosmerdovic <milos.merdovic1@gmail.com>
2022-07-23 04:24:45 +0200

    EMS-feature-002-added new views and optimized with controller

--- src/main/java/javamilos/com/example/demo/config/SecurityConfiguration.java
+++ src/main/java/javamilos/com/example/demo/config/SecurityConfiguration.java
@@ -40,7 +40,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(
-                        "/**","/registration**",
+                        "/**","/registration/**",
                         "/js/**",
                         "/css/**",
                         "/img/**").permitAll()
